f3613a39b05b8646015e7a37a59c42cec0af3397
Chu A. <chu.amk22@gmail.com>
2025-08-05 15:01:49 +0100

    chore(fix): fixing linter errors

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,16 +1,16 @@
-'use strict';
-const menuBtn = document.getElementById('menu-btn');
-const sideNav = document.getElementById('side-nav');
-const menu = document.getElementById('menu');
+'use strict'
+const menuBtn = document.getElementById('menu-btn')
+const sideNav = document.getElementById('side-nav')
+const menu = document.getElementById('menu')
 
-sideNav.style.right = '-250px';
+sideNav.style.right = '-250px'
 
 menuBtn.onclick = function () {
   if (sideNav.style.right === '-250px') {
-    sideNav.style.right = '0';
-    menu.src = 'assets/images/close.png';
+    sideNav.style.right = '0'
+    menu.src = 'assets/images/close.png'
   } else {
-    sideNav.style.right = '-250px';
-    menu.src = 'assets/images/menu.png';
+    sideNav.style.right = '-250px'
+    menu.src = 'assets/images/menu.png'
   }
 };
