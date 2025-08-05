c8274d3a6d8d1848e2ae2bd29773172b802e3fcc
Chu A. <chu.amk22@gmail.com>
2025-08-05 14:15:51 +0100

    chore(feat): implimented js for opening and closing navbar

---
+++ assets/scripts/script.js
@@ -0,0 +1,16 @@
+"use strict";
+let menuBtn = document.getElementById("menuBtn");
+let sideNav = document.getElementById("sideNav");
+let menu = document.getElementById("menu");
+
+sideNav.style.right = "-250px";
+
+menuBtn.onclick = function () {
+  if (sideNav.style.right == "-250px") {
+    sideNav.style.right = "0";
+    menu.src = "assets/images/close.png";
+  } else {
+    sideNav.style.right = "-250px";
+    menu.src = "assets/images/menu.png";
+  }
+};
