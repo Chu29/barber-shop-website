d302eb914a38bcf982f76a407710d19f3ba6e7d7
Chu A. <chu.amk22@gmail.com>
2025-08-05 14:57:45 +0100

    chore(fix): fixing linter errors

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,14 +3,14 @@ const menuBtn = document.getElementById('menu-btn');
 const sideNav = document.getElementById('side-nav');
 const menu = document.getElementById('menu');
 
-sideNav.style.right = "-250px";
+sideNav.style.right = '-250px';
 
 menuBtn.onclick = function () {
-    if (sideNav.style.right === '-250px') {
-        sideNav.style.right = '0';
-        menu.src = 'assets/images/close.png';
-    } else {
-        sideNav.style.right = '-250px';
-        menu.src = 'assets/images/menu.png';
-    }
+  if (sideNav.style.right === '-250px') {
+    sideNav.style.right = '0';
+    menu.src = 'assets/images/close.png';
+  } else {
+    sideNav.style.right = '-250px';
+    menu.src = 'assets/images/menu.png';
+  }
 };
